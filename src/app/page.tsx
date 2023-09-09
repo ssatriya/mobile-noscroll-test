2c73d9f4b119633a75c37d76df5e652bf62bcab4
ssatriya <ssatriya@gmail.com>
2023-09-09 18:32:45 +0800

    add css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
+    <main className="flex justify-center items-center relative">
       <h1 className="font-bold text-2xl">Hello</h1>
     </main>
   );
